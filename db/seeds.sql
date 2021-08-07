8f0869e39399c91c0861478401e12a7e751ad105
Emily N Mayeski <enmayeski@gmail.com>
2021-08-07 10:48:56 -0400

    table tests work correctly and basic schema sans references is set up

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Human Resources'),
+  ('Engineering'),
+  ('Product'),
+  ('Marketing');
+
+INSERT INTO role
+  (title, salary)
+VALUES
+  ('Engineer', '130000.00'),
+  ('Technical Writer', '100000.00'),
+  ('Product Manager', '120000.00'),
+  ('Marketing Manager', '90000.00'),
+  ('SCRUM Master', '100000.00'),
+  ('Lawyer', '250000.00'),
+  ('Social Media Manager', '80000.00');
+
+
+INSERT INTO employee
+  (first_name, last_name)
+VALUES
+  ('James', 'Fraser'),
+  ('Jack', 'London'),
+  ('Robert', 'Bruce'),
+  ('Peter', 'Greenaway'),
+  ('Derek', 'Jarman'),
+  ('Paolo', 'Pasolini'),
+  ('Heathcote', 'Williams'),
+  ('Sandy', 'Powell');
