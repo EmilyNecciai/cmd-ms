c9359b246e983e7a5226ff8c40c6311c7e1b049b
Emily N Mayeski <enmayeski@gmail.com>
2021-08-16 16:14:15 -0400

    fixed update employee functionality and figured out how to populate the Inquirer Arrays. Did so for AddEmployee function

--- assets/js/addEmployee.js
+++ assets/js/addEmployee.js
@@ -2,104 +2,113 @@
 
 const inquirer = require("inquirer");
 
-var roleArr = [];
-var employeeArr = [];
+var roleArr = {
+  id: [],
+  title: []
+};
+var employeeArr = {
+  id: [],
+  full_name: []
+};
 
+function roleList(connection) {
+  connection.query(
+    `
+  SELECT id, title FROM role
+      `,
+    function (err, result) {
+      if (err) throw err;
+      for (let i = 0; i < result.length; i++) {
+        roleArr.id.push(result[i].id);
+        roleArr.title.push(result[i].title);
+      }
+      return roleArr;
+    }
+  );
+}
 
-function roleList(connection){
+function employeeIdList(connection) {
   connection.query(`
-  SELECT id FROM role
-      `, 
-      function (err, result){
-          if (err) throw err;
-          for (let i = 0; i < result.length; i++) {
-              roleArr.push(result[i].id);
-          }
-          return roleArr;
-      })         
-  } 
+    SELECT  id, 
+            CONCAT (first_name, ' ', last_name) AS full_name 
+    FROM employee
+        `,
+    function (err, result) {
+      if (err) throw err;
+      for (let i = 0; i < result.length; i++) {
+        employeeArr.id.push(result[i].id);
+        employeeArr.full_name.push(result[i].full_name);
+      }
+      return employeeArr;
+    }
+  );
+}
 
-
-  function employeeIdList(connection){
-
-    connection.query(`
-    SELECT id FROM employee
-        `, 
-        function (err, result){
-            if (err) throw err;
-            for (let i = 0; i < result.length; i++) {
-              employeeArr.push(result[i].id);
-            }
-            return employeeArr;
-        })         
-    } 
-  
-
-function addEmployee(connection, menu){
+function addEmployee(connection, menu) {
   roleList(connection);
   employeeIdList(connection);
 
   inquirer
-  .prompt([
+    .prompt([
       {
-          type: "input",
-          name: "first_name",
-          message: "What is the first name of the new employee?",
-          validate: first_name => {
-            if (first_name) {
+        type: "input",
+        name: "first_name",
+        message: "What is the first name of the new employee?",
+        validate: (first_name) => {
+          if (first_name) {
             return true;
-            } else {
-            console.log('You need to input the first name of the new employee!');
+          } else {
+            console.log(
+              "You need to input the first name of the new employee!"
+            );
             return false;
-            }
-        }
+          }
+        },
       },
       {
         type: "input",
         name: "last_name",
         message: "What is the last name of the new employee?",
-        validate: last_name => {
+        validate: (last_name) => {
           if (last_name) {
-          return true;
+            return true;
           } else {
-          console.log('You need to input the last name of the new employee!');
-          return false;
+            console.log("You need to input the last name of the new employee!");
+            return false;
           }
-        }
+        },
       },
       {
         type: "list",
         name: "title",
         message: "What is the new employee's title?",
-        choices: roleArr
-        },
-        {
+        choices: roleArr.title,
+      },
+      {
         type: "list",
         name: "manager_id",
         message: "What is the id of the new employee's manager?",
-        choices: employeeArr
-        }
+        choices: employeeArr.full_name,
+      },
     ])
-    .then(answer => {
-      //Run cQ to get role id from title
+    .then((answer) => {
+      var roleId;
+      for (let i = 0; i < roleArr.title.length; i++) {
+        if (answer.title === roleArr.title[i]) {
+          roleId = roleArr.id[i];
+        }
+      }
+      // console.log(roleId);
 
-      // console.log(answer.title)
+      var empId;
+      for (let i = 0; i < employeeArr.full_name.length; i++) {
+        if (answer.manager_id === employeeArr.full_name[i]) {
+          empId = employeeArr.id[i];
+        }
+      }
 
-      // var roleId
-
-      // connection.query(
-      //   `SELECT id FROM role
-      //   WHERE title = ${JSON.stringify(answer.title)}
-      //   `,
-      //   function (err, roleId){
-      //     console.log(roleId[0].id)
-      //     // TO DO - pull in the role id for use in the next cQ
-      //     if (err) throw err;
-      //     return roleId;
-      //   }
-      // )
-
-      connection.query(`
+      connection.query(
+        `
       INSERT INTO employee (
           first_name, 
           last_name, 
@@ -108,18 +117,16 @@ function addEmployee(connection, menu){
         VALUES (
           ${JSON.stringify(answer.first_name)},
           ${JSON.stringify(answer.last_name)},
-          ${JSON.stringify(answer.title)},
-          ${JSON.stringify(answer.manager_id)})
+          ${JSON.stringify(roleId)},
+          ${JSON.stringify(empId)})
           `,
-          function (err, result){
-            if (err) throw err;
-            console.table(result)
-            menu();
-          }
-          )
+        function (err, result) {
+          if (err) throw err;
+          console.table(result);
+          menu();
         }
-        )
-      }
-    
-    
+      );
+    });
+}
+
 module.exports = addEmployee;
--- assets/js/updateEmployee.js
+++ assets/js/updateEmployee.js
@@ -1,74 +1,92 @@
 // Done, but something is broken.
- 
+
 const inquirer = require("inquirer");
 
 var employeeArr = [];
 
+// function employeeIdList(connection) {
+//   connection.query(
+//     `
+//   SELECT id FROM employee
+//       `,
+//     function (err, result) {
+//       if (err) throw err;
+//       for (let i = 0; i < result.length; i++) {
+//         employeeArr.push(result[i].id);
+//       }
+//       return employeeArr;
+//     }
+//   );
+// }
 
-function employeeIdList(connection){
+var roleArr = [];
 
-  connection.query(`
-  SELECT id FROM employee
-      `, 
-      function (err, result){
-          if (err) throw err;
-          for (let i = 0; i < result.length; i++) {
-            employeeArr.push(result[i].id);
-          }
-          return employeeArr;
-      })         
-  } 
+// function roleList(connection) {
+//   connection.query(
+//     `
+//   SELECT id FROM role
+//       `,
+//     function (err, result) {
+//       if (err) throw err;
+//       for (let i = 0; i < result.length; i++) {
+//         roleArr.push(result[i].id);
+//       }
+//       return roleArr;
+//     }
+//   );
+// }
 
-  var roleArr = [];
-
-function roleList(connection){
-  connection.query(`
-  SELECT id FROM role
-      `, 
-      function (err, result){
-          if (err) throw err;
-          for (let i = 0; i < result.length; i++) {
-              roleArr.push(result[i].id);
-          }
-          return roleArr;
-      })         
-  } 
-
-
-function updateEmployee(connection, menu){
-  roleList(connection);
-  employeeIdList(connection);
-    //   console.log(empArr)
-  inquirer
-  .prompt([
-      {
-        type: "list",
-        name: "id",
-        message: "Which employee would you like to update?",
-        choices: employeeArr
-        },
-        {
-        type: "list",
-        name: "title",
-        message: "What is the employee's new title?",
-        choices: roleArr
+function updateEmployee(connection, menu) {
+  // roleList(connection);
+  //employeeIdList(connection);
+  // console.log(employeeArr)
+  connection.query("SELECT * FROM employee", function (err, result) {
+    if (err) throw err;
+    for (let i = 0; i < result.length; i++) {
+      employeeArr.push(result[i].id);
+    }
+    connection.query(
+      `
+    SELECT id FROM role
+        `,
+      function (err, result) {
+        if (err) throw err;
+        for (let i = 0; i < result.length; i++) {
+          roleArr.push(result[i].id);
         }
-    ])
-    .then(answer => {
-      connection.query(`
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "id",
+              message: "Which employee would you like to update?",
+              choices: employeeArr,
+            },
+            {
+              type: "list",
+              name: "title",
+              message: "What is the employee's new title?",
+              choices: roleArr,
+            },
+          ])
+          .then((answer) => {
+            connection.query(
+              `
         UPDATE employee 
         SET role_id = ${JSON.stringify(answer.title)} 
         WHERE id = ${JSON.stringify(answer.id)}
-        )`,
-          function (err, result){
-            if (err) throw err;
-            console.table(result)
-            menu();
-          }
-          )
-        }
-        )
+        `,
+              function (err, result) {
+                if (err) throw err;
+                console.table(result);
+                menu();
+              }
+            );
+          });
       }
-
+    );
+  });
+}
 
 module.exports = updateEmployee;
